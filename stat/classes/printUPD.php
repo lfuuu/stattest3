4ac5266b12a8a75e7a31ff7d3909e362d7e19200
Андреев Дмитрий <adima@mcn.ru>
2026-01-19 19:05:16 +0300

    UPD2. Stamp. preview

--- stat/classes/printUPD.php
+++ stat/classes/printUPD.php
@@ -4,19 +4,17 @@
 class printUPD
 {
 
-    private static $pageSize = 750;
-    private static $defaultRowSize = 29;
+    private static $pageSize = 670;
+    private static $defaultRowSize = 30;
 
     public static function getInfo($positions, $rowSize = null)
     {
-        if ($rowSize === null)
-        {
+        if ($rowSize === null) {
             $rowSize = self::$defaultRowSize;
         }
         $changeSize = get_param_integer('is_pdf', 0);
-        if (!$changeSize)
-        {
-		self::changePageSize(720);
+        if (!$changeSize) {
+            self::changePageSize(720);
         }
 
         $page = self::constructPage($positions, $rowSize);
@@ -24,33 +22,28 @@ class printUPD
         $size = 0;
         $pageNum = 1;
         $newPageLineIndex = array();
-        foreach($page as $k => $pobj)
-        {
-            if (defined("print_debug"))
-            {
+        foreach ($page as $k => $pobj) {
+            if (defined("print_debug")) {
                 echo "\n--------------------";
-                echo "\n".$pobj["obj"];
-                echo "\nsize: ".$size. " (rowSize: ".$rowSize.")";
-                echo "\nsize+p[size]: ".($size + $pobj["size"]);
-                echo "\npage: ".$pageNum.", pageSize: ".($pageNum*self::$pageSize);
-            }
-            
-            if ($pobj["obj"] == "line")
-            {
-		$newPageLineIndex[$k-1] = false;
+                echo "\n" . $pobj["obj"];
+                echo "\nsize: " . $size . " (rowSize: " . $rowSize . ")";
+                echo "\nsize+p[size]: " . ($size + $pobj["size"]);
+                echo "\npage: " . $pageNum . ", pageSize: " . ($pageNum * self::$pageSize);
             }
 
-            $isNewPage = ($size + $pobj["size"] >= $pageNum*self::$pageSize);
+            if ($pobj["obj"] == "line") {
+                $newPageLineIndex[$k - 1] = false;
+            }
 
-            if ($isNewPage)
-            {
+            $isNewPage = ($size + $pobj["size"] >= $pageNum * self::$pageSize);
+
+            if ($isNewPage) {
                 // if footer or last product line on 2 pages
-                if ($pobj["obj"] == "footer" || ($pobj["obj"] == "line" && $pobj["is_last"]))
-                {
-                    return self::getInfo($positions, $rowSize+1);
+                if ($pobj["obj"] == "footer" || ($pobj["obj"] == "line" && $pobj["is_last"] && $rowSize < 100)) {
+                    return self::getInfo($positions, $rowSize + 1);
                 } else {
-		     $newPageLineIndex[$k-1] = $pageNum*self::$pageSize-$size;
-		     $size = $pageNum*self::$pageSize;
+                    $newPageLineIndex[$k - 1] = $pageNum * self::$pageSize - $size;
+                    $size = $pageNum * self::$pageSize;
                 }
                 $pageNum++;
             }
@@ -62,23 +55,23 @@ class printUPD
     private static function constructPage($positions, $rowSize)
     {
         $page = array();
-        $page[] = array("obj" => "header", "size" => self::$defaultRowSize*10);
+        $page[] = array("obj" => "header", "size" => self::$defaultRowSize * 11);
 
-        for($i=1 ; $i <= $positions; $i++)
-        {
+        for ($i = 1; $i <= $positions; $i++) {
             $page[] = array("obj" => "line", "size" => $rowSize, "is_last" => false);
         }
 
         if ($positions)
-            $page[count($page)-1]["is_last"] = true;
+            $page[count($page) - 1]["is_last"] = true;
 
-        $page[] = array("obj" => "footer", "size" => self::$defaultRowSize*10);
-        
+        $page[] = array("obj" => "footer", "size" => self::$defaultRowSize * 10);
+
         return $page;
     }
+
     private static function changePageSize($size)
     {
-	self::$pageSize = $size;
+        self::$pageSize = $size;
     }
 }
 
