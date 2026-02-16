e20481daff252495802ac707dfa95fc1980f8d43
Андреев Дмитрий <adima@mcn.ru>
2026-02-16 15:52:36 +0300

    Автоскалинг событий в очереди.

--- stat/crons/events/launcher.php
+++ stat/crons/events/launcher.php
@@ -67,7 +67,7 @@ Param::setParam("handler_{$group}_total", $workersNeeded, true);
 // запускаем воркеры (argv[2] = индекс воркера, N берётся из БД)
 $scriptDir = __DIR__;
 for ($i = 1; $i <= $workersNeeded; $i++) {
-    $lockFile = "/var/run/handler_{$group}_{$i}";
+    $lockFile = "/tmp/handler_{$group}_{$i}";
     $cmd = sprintf(
         'flock --nonblock %s php %s/handler.php %s %d >> /var/log/nispd/handler_%s.log 2>&1 &',
         escapeshellarg($lockFile),
