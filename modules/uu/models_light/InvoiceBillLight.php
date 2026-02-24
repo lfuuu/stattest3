bcc6a3db02c877646e3d643c6240471b480287aa
Daniil Oleynik <lfuuu@yandex.ru>
2026-02-24 10:01:16 +0000

    fix

--- modules/uu/models_light/InvoiceBillLight.php
+++ modules/uu/models_light/InvoiceBillLight.php
@@ -252,6 +252,7 @@ class InvoiceBillLight extends Component implements InvoiceLightInterface
                 $contractDate = $contractDateTime->format(DateTimeZoneHelper::DATE_FORMAT);
                 $contractDateHuman = $contractDateTime->format(DateTimeZoneHelper::DATE_FORMAT_EUROPE_DOTTED);
                 $contractNumber = (string)$contract->contract_no;
+                $contractName = sprintf('Договор №%s от %s', $contractNumber, $contractDateHuman);
 
                 return [
                     'type' => ClientAccountOptions::OPTION_SBIS_DOC_BASE_CONTRACT,
@@ -259,8 +260,8 @@ class InvoiceBillLight extends Component implements InvoiceLightInterface
                     'number' => $contractNumber,
                     'date' => $contractDate,
                     'date_human' => $contractDateHuman,
-                    'sbis_name' => sprintf('Договор №%s от %s', $contractNumber, $contractDateHuman),
-                    'upd_name' => sprintf('Договор №%s от %s', $contractNumber, $contractDateHuman),
+                    'sbis_name' => $contractName,
+                    'upd_name' => $contractName,
                 ];
 
             default:
