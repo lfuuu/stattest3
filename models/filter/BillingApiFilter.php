f11b88c6de73e5ce0ee2dbb115c051b4534b5522
Daniil Oleynik <lfuuu@yandex.ru>
2026-03-23 13:40:57 +0000

    Статистика API. Период по умолчанию, группировка по методам, переключение месяцев

--- models/filter/BillingApiFilter.php
+++ models/filter/BillingApiFilter.php
@@ -3,7 +3,10 @@
 namespace app\models\filter;
 
 use app\classes\grid\ActiveDataProvider;
+use app\helpers\DateTimeZoneHelper;
 use app\models\billing\api\ApiRaw;
+use DateTimeImmutable;
+use DateTimeZone;
 use Yii;
 use yii\db\Expression;
 
@@ -11,6 +14,9 @@ class BillingApiFilter extends ApiRaw
 {
     public $accountId = null;
     public $isLoad = false;
+    public $group_by_method = 0;
+    public $api_weight_total = null;
+    public $cost_total = null;
 
     public
         $connect_time_from = '',
@@ -31,7 +37,7 @@ class BillingApiFilter extends ApiRaw
         $fieldList = ['connect_time_from', 'connect_time_to', 'api_weight_from', 'api_weight_to', 'rate_from', 'rate_to', 'cost_from', 'cost_to'];
         return array_merge(parent::rules(), [
             [$fieldList, 'required'],
-            [array_merge($fieldList, ['api_method_id']), 'string'],
+            [array_merge($fieldList, ['api_method_id', 'group_by_method']), 'string'],
         ]);
     }
 
@@ -43,27 +49,53 @@ class BillingApiFilter extends ApiRaw
     {
         $this->accountId = $clientId;
 
-        parent::load(Yii::$app->request->get());
+        $requestData = Yii::$app->request->get();
+
+        parent::load($requestData);
+
+        $filterData = $requestData[$this->formName()] ?? [];
+        if (empty($filterData['connect_time_from']) && empty($filterData['connect_time_to'])) {
+            $currentMonth = new DateTimeImmutable('now', new DateTimeZone(DateTimeZoneHelper::TIMEZONE_UTC));
+            $this->connect_time_from = $currentMonth->modify('first day of this month')->format(DateTimeZoneHelper::DATE_FORMAT);
+            $this->connect_time_to = $currentMonth->modify('last day of this month')->format(DateTimeZoneHelper::DATE_FORMAT);
+        }
 
         return $this;
     }
 
-    private function makeQuery()
+    public function isGroupByMethod(): bool
     {
-        $query = ApiRaw::find()->with('method');
+        return (bool)$this->group_by_method;
+    }
 
-        if (!($this->connect_time_from && $this->connect_time_to && $this->accountId)) {
+    private function makeQuery(bool $withGrouping = true)
+    {
+        $query = self::find()->with('method');
+
+        if (!($this->connect_time_from && $this->connect_time_to)) {
             $query->andWhere('false');
         } else {
             $this->isLoad = true;
             $query->andWhere(['between', 'connect_time', $this->connect_time_from . ' 00:00:00', $this->connect_time_to . ' 23:59:59.999999']);
         }
 
-        $query->andWhere(['account_id' => $this->accountId]);
+        if ($this->accountId) {
+            $query->andWhere(['account_id' => $this->accountId]);
+        }
 
         $this->api_method_id && $query->andWhere(['api_method_id' => $this->api_method_id]);
         $this->api_weight_from && $query->andWhere(['>=', 'api_weight', $this->api_weight_from]);
         $this->api_weight_to && $query->andWhere(['<=', 'api_weight', $this->api_weight_to]);
+
+        if ($withGrouping && $this->isGroupByMethod()) {
+            $query
+                ->select([
+                    'api_method_id',
+                    'api_weight_total' => new Expression('sum(api_weight)'),
+                    'cost_total' => new Expression('-sum(cost)'),
+                ])
+                ->groupBy(['api_method_id']);
+        }
 
         return $query;
     }
@@ -73,14 +105,51 @@ class BillingApiFilter extends ApiRaw
      */
     public function search()
     {
+        $sort = $this->isGroupByMethod()
+            ? [
+                'defaultOrder' => [
+                    'api_method_id' => SORT_ASC,
+                ],
+                'attributes' => [
+                    'api_method_id',
+                    'api_weight_total' => [
+                        'asc' => ['api_weight_total' => SORT_ASC],
+                        'desc' => ['api_weight_total' => SORT_DESC],
+                        'default' => SORT_DESC,
+                    ],
+                    'cost_total' => [
+                        'asc' => ['cost_total' => SORT_ASC],
+                        'desc' => ['cost_total' => SORT_DESC],
+                        'default' => SORT_DESC,
+                    ],
+                ],
+            ]
+            : [
+                'defaultOrder' => [
+                    'id' => SORT_ASC,
+                ],
+                'attributes' => [
+                    'id',
+                    'connect_time',
+                    'api_method_id',
+                    'api_weight',
+                    'rate' => [
+                        'asc' => ['rate' => SORT_ASC],
+                        'desc' => ['rate' => SORT_DESC],
+                        'default' => SORT_DESC,
+                    ],
+                    'cost' => [
+                        'asc' => ['cost' => SORT_DESC],
+                        'desc' => ['cost' => SORT_ASC],
+                        'default' => SORT_DESC,
+                    ],
+                ],
+            ];
+
         $dataProvider = new ActiveDataProvider([
             'query' => $this->makeQuery(),
             'db' => ApiRaw::getDb(),
-            'sort' => [
-                'defaultOrder' => [
-                    'id' => SORT_ASC,
-                ]
-            ],
+            'sort' => $sort,
         ]);
 
         return $dataProvider;
@@ -88,7 +157,7 @@ class BillingApiFilter extends ApiRaw
 
     public function getTotal()
     {
-        $query = $this->makeQuery();
+        $query = $this->makeQuery(false);
 
         $query->select(['sum' => new Expression('-sum(cost)')]);
 
--- views/stats/billing-api/index.php
+++ views/stats/billing-api/index.php
@@ -1,21 +1,22 @@
 <?php
 /**
- * Вознаграждения партнеров
+ * Статистика: Вызовы-API
  *
  * @var \app\classes\BaseView $this
  * @var BillingApiFilter $filterModel
  */
 
 use app\classes\grid\column\universal\DateRangeDoubleColumn;
-use app\classes\grid\column\universal\DateTimeRangeDoubleColumn;
 use app\classes\grid\column\universal\DropdownColumn;
 use app\classes\grid\column\universal\IntegerRangeColumn;
-use app\classes\grid\column\universal\StringColumn;
 use app\classes\grid\GridView;
 use app\classes\Html;
+use app\helpers\DateTimeZoneHelper;
 use app\models\billing\api\ApiMethod;
 use app\models\billing\api\ApiRaw;
 use app\models\filter\BillingApiFilter;
+use DateTimeImmutable;
+use DateTimeZone;
 use yii\helpers\Url;
 use yii\widgets\ActiveForm;
 use yii\widgets\Breadcrumbs;
@@ -29,10 +30,37 @@ echo Breadcrumbs::widget([
     ],
 ]);
 
-if (!$filterModel->accountId) {
-    echo Html::tag('div', 'Клиент не выбран', ['class' => 'alert alert-danger']);
-    return;
-}
+$utcNow = new DateTimeImmutable('now', new DateTimeZone(DateTimeZoneHelper::TIMEZONE_UTC));
+$requestParams = Yii::$app->request->queryParams;
+unset($requestParams['page'], $requestParams['sort']);
+
+$filterParams = $requestParams[$filterModel->formName()] ?? [];
+$buildPeriodUrl = function (string $dateFrom, string $dateTo) use ($baseUrl, $filterModel, $requestParams, $filterParams) {
+    $requestParams[$filterModel->formName()] = array_merge($filterParams, [
+        'connect_time_from' => $dateFrom,
+        'connect_time_to' => $dateTo,
+    ]);
+
+    return Url::to(array_merge([$baseUrl], $requestParams));
+};
+
+$periodAnchor = new DateTimeImmutable(
+    $filterModel->connect_time_from ?: $utcNow->format(DateTimeZoneHelper::DATE_FORMAT),
+    new DateTimeZone(DateTimeZoneHelper::TIMEZONE_UTC)
+);
+$previousMonth = $periodAnchor->modify('first day of previous month');
+$currentPeriodMonth = $periodAnchor->modify('first day of this month');
+$today = $utcNow->format(DateTimeZoneHelper::DATE_FORMAT);
+
+$previousMonthUrl = $buildPeriodUrl(
+    $previousMonth->format('Y-m-01'),
+    $previousMonth->format('Y-m-t')
+);
+$currentMonthUrl = $buildPeriodUrl(
+    $currentPeriodMonth->format('Y-m-01'),
+    $currentPeriodMonth->format('Y-m-t')
+);
+$currentDayUrl = $buildPeriodUrl($today, $today);
 
 if (!$filterModel->isLoad) {
     ?>
@@ -47,36 +75,97 @@ if (!$filterModel->isLoad) {
 }
 $form = ActiveForm::begin(['method' => 'get', 'action' => $baseUrl]);
 
-$columns = [
-    [
-        'attribute' => 'connect_time',
-        'class' => DateRangeDoubleColumn::class,
-    ],
-    [
-        'attribute' => 'api_method_id',
-        'class' => DropdownColumn::class,
-        'filter' => ApiMethod::getList(true),
-        'value' => function (ApiRaw $row) {
-            return $row->method->name;
-        }
-    ],
-    [
-        'attribute' => 'api_weight',
-        'class' => IntegerRangeColumn::class,
-    ],
-    [
-        'attribute' => 'rate',
-        'class' => IntegerRangeColumn::class,
-    ],
-    [
-        'attribute' => 'cost',
-        'class' => IntegerRangeColumn::class,
-        'value' => function (ApiRaw $row) {
-            return -$row->cost;
-        }
-    ]
+?>
 
-];
+<div style="margin-bottom: 15px;">
+    Создайте отчёт сами:
+    (или - посмотрите отчёты за
+    <?= Html::a('прошлый месяц', $previousMonthUrl) ?>,
+    за <?= Html::a('текущий месяц', $currentMonthUrl) ?>,
+    за <?= Html::a('текущий день', $currentDayUrl) ?>)
+</div>
+
+<?php
+?>
+
+<div class="row" style="margin-bottom: 15px;">
+    <div class="col-sm-4">
+        <label style="font-weight: normal; margin-top: 7px;">
+            <?= Html::activeCheckbox($filterModel, 'group_by_method', ['label' => false, 'value' => 1, 'uncheck' => 0]) ?>
+            Группировка по API-методам
+        </label>
+    </div>
+    <div class="col-sm-3">
+        <?= Html::submitButton('Сформировать отчёт', ['class' => 'btn btn-primary btn-sm']) ?>
+    </div>
+</div>
+
+<?php
+
+if ($filterModel->isGroupByMethod()) {
+    $columns = [
+        [
+            'attribute' => 'connect_time',
+            'label' => 'Период, UTC',
+            'class' => DateRangeDoubleColumn::class,
+            'value' => function () use ($filterModel) {
+                return $filterModel->connect_time_from . ' - ' . $filterModel->connect_time_to;
+            }
+        ],
+        [
+            'attribute' => 'api_method_id',
+            'class' => DropdownColumn::class,
+            'filter' => ApiMethod::getList(true),
+            'value' => function (ApiRaw $row) {
+                return $row->method ? $row->method->name : null;
+            }
+        ],
+        [
+            'attribute' => 'api_weight_total',
+            'label' => 'Суммарный вес вызова',
+            'value' => function (ApiRaw $row) {
+                return $row->api_weight_total;
+            }
+        ],
+        [
+            'attribute' => 'cost_total',
+            'label' => 'Суммарная стоимость',
+            'value' => function (ApiRaw $row) {
+                return round((float)$row->cost_total, 3);
+            }
+        ],
+    ];
+} else {
+    $columns = [
+        [
+            'attribute' => 'connect_time',
+            'class' => DateRangeDoubleColumn::class,
+        ],
+        [
+            'attribute' => 'api_method_id',
+            'class' => DropdownColumn::class,
+            'filter' => ApiMethod::getList(true),
+            'value' => function (ApiRaw $row) {
+                return $row->method ? $row->method->name : null;
+            }
+        ],
+        [
+            'attribute' => 'api_weight',
+            'class' => IntegerRangeColumn::class,
+        ],
+        [
+            'attribute' => 'rate',
+            'class' => IntegerRangeColumn::class,
+        ],
+        [
+            'attribute' => 'cost',
+            'class' => IntegerRangeColumn::class,
+            'value' => function (ApiRaw $row) {
+                return -$row->cost;
+            }
+        ]
+    ];
+}
 
 echo GridView::widget([
     'dataProvider' => $filterModel->search(),
