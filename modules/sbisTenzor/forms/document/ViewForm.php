780340d549c0db5d3f15a2bc7c858e9667622791
Daniil Oleynik <lfuuu@yandex.ru>
2026-03-30 09:05:49 +0000

    Кнопка пересоздать при статусе создан или создан(Авто)

--- modules/sbisTenzor/forms/document/ViewForm.php
+++ modules/sbisTenzor/forms/document/ViewForm.php
@@ -129,7 +129,9 @@ class ViewForm extends \app\classes\Form
         }
 
         return
-            $document->state == SBISDocumentStatus::NEGOTIATED
+            $document->state == SBISDocumentStatus::CREATED
+            || $document->state == SBISDocumentStatus::CREATED_AUTO
+            || $document->state == SBISDocumentStatus::NEGOTIATED
             || $document->state == SBISDocumentStatus::ERROR
             || $document->state == SBISDocumentStatus::SENT
             || $document->state == SBISDocumentStatus::SENT_ERROR
